2c8339560cb5fab78c02ee27bcaede537d458d57
parking52 <melchior.fracas@wanadoo.fr>
2016-09-04 16:27:36 +0200

    set all to good sets

--- mafr/load_data_into_array.py
+++ mafr/load_data_into_array.py
@@ -5,13 +5,18 @@ import csv
 import os
 import numpy as np
 
-my_data = np.genfromtxt(os.path.join('..', 'data', 'train.csv'), delimiter=',')
+data = np.genfromtxt(os.path.join('..', 'data', 'train.csv'), delimiter=',')
 
-print(my_data)
-
-columns = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
-
-df = [my_data, columns]
+columns = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+df = [data, columns]
 
 print(df[0])
 
+test_set = np.genfromtxt(os.path.join('..', 'data', 'test.csv'), delimiter=',')
+test_columns = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin','Embarked']
+
+train_set = np.delete(data, 1, axis=1)
+train_result = data[:, 1]
+test_set = test_set
+test_guess = np.zeros(train_set.shape[0])
+pass
