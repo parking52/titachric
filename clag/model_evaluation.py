1aca089fd076a9d6edd95c37cd7250a6f416d819
chris <hasensilvester@gmail.com>
2016-10-09 19:10:49 +0200

    pandas rules

--- clag/model_evaluation.py
+++ clag/model_evaluation.py
@@ -1,15 +1,17 @@
 
 import pandas as pd
-from clag.data_container import LassoModel
+from clag.model_container import LassoModel
 
 
 
-test_data = pd.read_csv('../data/header_test.csv')
+test_data = pd.read_csv('/home/chris/python_projects/titachric/data/header_test.csv')
+train_data = pd.read_csv('/home/chris/python_projects/titachric/data/header_train.csv')
+
 
 def evaluate(model_name):
-    model = model_name()
-    model.train()
-    predictions = model.predict(test_data[model.features])
+    model_instance = model_name()
+    model_instance.train(train_data)
+    predictions = model_instance.predict(model_instance.model, train_data)
     print(predictions)
 
 
