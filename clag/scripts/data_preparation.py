f7405081ece773225b83872b8da52fa31ddc4676
chris <hasensilvester@gmail.com>
2016-10-26 00:13:38 +0200

    more granular class seperation

--- clag/scripts/data_preparation.py
+++ clag/scripts/data_preparation.py
@@ -18,12 +18,19 @@ class PassengerData(object):
     def process_training_data(self):
         self.process_age()
         self.process_sex()
+        self.process_passenger_class()
 
     def process_age(self):
         self.training_data.Age.fillna(25, inplace=True)
 
     def process_sex(self):
         self.training_data['Sex'] = self.training_data['Sex'].map({'male': 1, 'female': 0})
+
+    def process_passenger_class(self):
+        for class_number in [1, 2, 3]:
+            column_name = "Class_" + str(class_number)
+            self.training_data.loc[self.training_data['Pclass'] == class_number, column_name] = 1
+            self.training_data.loc[self.training_data['Pclass'] != class_number, column_name] = 0
 
     def save_data(self):
         self.training_data.to_pickle(CLEANED_DATA_FILEPATH)
