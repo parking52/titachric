d8e55e138e866e6e33d299740491d1af7a231fa3
Christoph Lange <christoph.lange2@zalando.de>
2016-11-11 14:45:41 +0100

    resolve conflicts

--- clag/models/linear_classifier_with_lasso.py
+++ clag/models/linear_classifier_with_lasso.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Lasso, LogisticRegression
 from clag.models.BaseClassModel import BaseClassModel
 
 list_of_parameters = np.logspace((-3),2, num = 6)
 list_of_parameters = [0, 0.01, 0.2, 0.5, 1 ]
 
 
-class LassoModel(BaseClassModel):
 
+class LassoModel(BaseClassModel):
     def __init__(self):
         super(LassoModel, self).__init__()
         self.features = ['Age', 'Fare', 'Sex', 'Pclass']
@@ -24,7 +24,7 @@ class LassoModel(BaseClassModel):
             errors.append((parameter, np.mean(prediction_errors)))
             print(str(model.coef_) + '    ' + str(parameter))
 
-        errors.sort(key= lambda x: x[1])
+        errors.sort(key=lambda x: x[1])
         print(errors)
 
         optimal_parameter = errors[0][0]
@@ -37,6 +37,27 @@ class LassoModel(BaseClassModel):
         values[values < 0.5] = 0
         values[values >= 0.5] = 1
         return values
+
+
+class LogisticModel(BaseClassModel):
+    def __init__(self):
+        super(LogisticModel, self).__init__()
+        self.features = ['Age', 'Fare']
+        self.model = None
+        self.optimal_parameter = None
+
+    def train(self):
+        errors = []
+        model = LogisticModel()
+        prediction_errors = self.cross_validation(self.training_data, model)
+        errors.append((parameter, np.mean(prediction_errors)))
+
+        errors.sort(key=lambda x: x[1])
+        print(errors)
+        optimal_parameter = errors[0][0]
+        self.optimal_parameter = optimal_parameter
+        self.model = Lasso(alpha=optimal_parameter)
+        self.model.fit(self.get_X(self.features), self.get_Y())
 
 
 if __name__ == '__main__':
